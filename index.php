faf2a411b678980fe7d0a31cdb2765820b82e8e9
Vahid Almasi <vahid.almasi71@gmail.com>
2019-01-07 06:23:44 +0330

    change the index file and add some new out put

--- index.php
+++ index.php
@@ -8,11 +8,19 @@ $shapes = array(
     new Square(6)
 );
 
+$solidShapes = array(
+    new sphere(2),
+    new cuboid(5),
+    new cuboid(6)
+);
+
 $areas = new AreaCalculator($shapes);
+$volumes = new AreaCalculator($solidShapes);
+
 $output = new SumCalculatorOutputter($areas);
+$output2 = new SumCalculatorOutputter($volumes);
 
 echo $output->JSON();
 echo $output->HAML();
-echo $output->HTML();
-echo $output->JADE();
-
+echo $output2->HTML();
+echo $output2->JADE();
